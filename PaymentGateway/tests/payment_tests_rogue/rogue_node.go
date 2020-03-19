648c61792da4b6d252b66efbc9d2b411fbe74c6c
Michael Kanevsky <michael@adaptivelogic.net>
2020-03-19 10:30:22 +0200

    adding bad signature test using rogue node framework

--- PaymentGateway/tests/payment_tests_rogue/rogue_node.go
+++ PaymentGateway/tests/payment_tests_rogue/rogue_node.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-errors/errors"
 	"github.com/stellar/go/build"
 	"github.com/stellar/go/clients/horizon"
+	"github.com/stellar/go/keypair"
 	"github.com/stellar/go/txnbuild"
 	"github.com/stellar/go/xdr"
 	"paidpiper.com/payment-gateway/common"
@@ -12,6 +13,8 @@ import (
 
 type RogueNode struct {
 	internalNode node.PPNode
+	transactionCreationFunction func(*RogueNode,common.TransactionAmount,common.TransactionAmount,common.TransactionAmount, string) (common.PaymentTransactionReplacing,error)
+	signChainTransactionsFunction func(*RogueNode, *common.PaymentTransactionReplacing, *common.PaymentTransactionReplacing) *errors.Error
 }
 
 func (r *RogueNode) AddPendingServicePayment(serviceSessionId string, amount common.TransactionAmount) {
@@ -27,6 +30,39 @@ func (r *RogueNode) CreatePaymentRequest(serviceSessionId string) (common.Paymen
 }
 
 func (r *RogueNode) CreateTransaction(totalIn common.TransactionAmount, fee common.TransactionAmount, totalOut common.TransactionAmount, sourceAddress string) (common.PaymentTransactionReplacing,error) {
+
+	return r.transactionCreationFunction(r, totalIn, fee, totalOut, sourceAddress)
+}
+
+func createTransactionCorrect(r *RogueNode,totalIn common.TransactionAmount, fee common.TransactionAmount, totalOut common.TransactionAmount, sourceAddress string) (common.PaymentTransactionReplacing,error) {
+	return r.internalNode.CreateTransaction(totalIn, fee, totalOut, sourceAddress)
+}
+
+func (r *RogueNode) SignTerminalTransactions(creditTransactionPayload *common.PaymentTransactionReplacing) *errors.Error {
+	return r.internalNode.SignTerminalTransactions(creditTransactionPayload)
+}
+
+func (r *RogueNode) SignChainTransactions(creditTransactionPayload *common.PaymentTransactionReplacing, debitTransactionPayload *common.PaymentTransactionReplacing) *errors.Error {
+	return r.signChainTransactionsFunction(r,creditTransactionPayload,debitTransactionPayload)
+}
+
+func signChainTransactionsNoError(r *RogueNode,creditTransactionPayload *common.PaymentTransactionReplacing, debitTransactionPayload *common.PaymentTransactionReplacing) *errors.Error {
+	return r.internalNode.SignChainTransactions(creditTransactionPayload,debitTransactionPayload)
+}
+
+func (r *RogueNode) CommitServiceTransaction(transaction *common.PaymentTransactionReplacing, pr common.PaymentRequest) (bool, error) {
+	return r.internalNode.CommitServiceTransaction(transaction,pr)
+}
+
+func (r *RogueNode) CommitPaymentTransaction(transactionPayload *common.PaymentTransactionReplacing) (ok bool, err error) {
+	return r.internalNode.CommitPaymentTransaction(transactionPayload)
+}
+
+func (r *RogueNode) GetAddress() string {
+	return r.internalNode.GetAddress()
+}
+
+func createTransactionIncorrectSequence(r *RogueNode,totalIn common.TransactionAmount, fee common.TransactionAmount, totalOut common.TransactionAmount, sourceAddress string) (common.PaymentTransactionReplacing,error) {
 	transaction,err := r.internalNode.CreateTransaction(totalIn,fee,totalOut,sourceAddress)
 
 	if err != nil {
@@ -55,15 +91,15 @@ func (r *RogueNode) CreateTransaction(totalIn common.TransactionAmount, fee comm
 	var payment *txnbuild.Payment
 
 	switch xdrOp.Body.Type {
-		case xdr.OperationTypePayment:
-			payment = &txnbuild.Payment{}
-			err = payment.FromXDR(xdrOp)
-			if err != nil {
-				panic("error deserializing op xdr")
-			}
+	case xdr.OperationTypePayment:
+		payment = &txnbuild.Payment{}
+		err = payment.FromXDR(xdrOp)
+		if err != nil {
+			panic("error deserializing op xdr")
+		}
 
-		default:
-			panic("unexpected operation type")
+	default:
+		panic("unexpected operation type")
 
 	}
 
@@ -95,24 +131,56 @@ func (r *RogueNode) CreateTransaction(totalIn common.TransactionAmount, fee comm
 	return transaction,nil
 }
 
-func (r *RogueNode) SignTerminalTransactions(creditTransactionPayload *common.PaymentTransactionReplacing) *errors.Error {
-	return r.internalNode.SignTerminalTransactions(creditTransactionPayload)
-}
+func signChainTransactionsBadSignature(r *RogueNode,creditTransactionPayload *common.PaymentTransactionReplacing, debitTransactionPayload *common.PaymentTransactionReplacing) *errors.Error {
 
-func (r *RogueNode) SignChainTransactions(creditTransactionPayload *common.PaymentTransactionReplacing, debitTransactionPayload *common.PaymentTransactionReplacing) *errors.Error {
-	return r.internalNode.SignChainTransactions(creditTransactionPayload,debitTransactionPayload)
-}
+	creditTransaction := creditTransactionPayload.GetPaymentTransaction()
+	debitTransaction := debitTransactionPayload.GetPaymentTransaction()
 
-func (r *RogueNode) CommitServiceTransaction(transaction *common.PaymentTransactionReplacing, pr common.PaymentRequest) (bool, error) {
-	return r.internalNode.CommitServiceTransaction(transaction,pr)
-}
+	kp, err := keypair.Random()
 
-func (r *RogueNode) CommitPaymentTransaction(transactionPayload *common.PaymentTransactionReplacing) (ok bool, err error) {
-	return r.internalNode.CommitPaymentTransaction(transactionPayload)
-}
+	credit, err := txnbuild.TransactionFromXDR(creditTransaction.XDR)
+	credit.Network = creditTransaction.StellarNetworkToken
 
-func (r *RogueNode) GetAddress() string {
-	return r.internalNode.GetAddress()
+	if err != nil {
+		return errors.New("Transaction deser error")
+	}
+
+	debit, err := txnbuild.TransactionFromXDR(debitTransaction.XDR)
+	debit.Network = debitTransaction.StellarNetworkToken
+
+	if err != nil {
+		return errors.New("Transaction parse error")
+	}
+
+	err = credit.Sign(kp)
+
+	if err != nil {
+		return errors.New("Failed to sign transaction")
+	}
+
+	err = debit.Sign(kp)
+
+	if err != nil {
+		return errors.New("Failed to sign transaction")
+	}
+
+	creditTransaction.XDR, err = credit.Base64()
+
+	if err != nil {
+		return errors.New("Transaction envelope error")
+	}
+
+	creditTransactionPayload.UpdateTransactionXDR(creditTransaction.XDR)
+
+	debitTransaction.XDR, err = debit.Base64()
+
+	if err != nil {
+		return errors.New("Transaction envelope error")
+	}
+
+	debitTransactionPayload.UpdateTransactionXDR(debitTransaction.XDR)
+
+	return nil
 }
 
 func CreateRogueNode_NonidenticalSequenceNumbers(client *horizon.Client,address string, seed string, accumulateTransactions bool) node.PPNode {
@@ -121,6 +189,21 @@ func CreateRogueNode_NonidenticalSequenceNumbers(client *horizon.Client,address 
 
 	rogueNode := RogueNode {
 		internalNode:node,
+		transactionCreationFunction:createTransactionIncorrectSequence,
+		signChainTransactionsFunction:signChainTransactionsNoError,
+	}
+
+	return &rogueNode
+}
+
+func CreateRogueNode_BadSignature(client *horizon.Client,address string, seed string, accumulateTransactions bool) node.PPNode {
+
+	node := node.CreateNode(client,address,seed,accumulateTransactions)
+
+	rogueNode := RogueNode {
+		internalNode:node,
+		transactionCreationFunction:createTransactionCorrect,
+		signChainTransactionsFunction:signChainTransactionsBadSignature,
 	}
 
 	return &rogueNode

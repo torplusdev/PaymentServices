cadb3028d8e806ea8722eca0fecacf1c868e5a51
Michael Kanevsky <michael@adaptivelogic.net>
2020-08-25 19:59:25 +0300

    Added configuration file support

---
+++ PaymentGateway/common/config.go
@@ -0,0 +1,61 @@
+package common
+
+import (
+	"github.com/go-errors/errors"
+	"sync"
+	"time"
+)
+import "github.com/tkanos/gonfig"
+
+var once sync.Once
+
+type jsonCnfiguration struct {
+	Port              int
+	StellarSeed		  string
+	JaegerUrl		  string
+	JaegerServiceName string
+	AutoFlushPeriod	  string
+	MaxConcurrency	  int
+}
+
+type configuration struct {
+	Port              int
+	StellarSeed		  string
+	JaegerUrl		  string
+	JaegerServiceName string
+	AutoFlushPeriod	  time.Duration
+	MaxConcurrency	  int
+}
+
+
+
+var (
+	instance configuration
+)
+
+func ParseConfiguration(configFile string) (configuration,error) {
+
+	rawConfig := jsonCnfiguration{}
+
+	err := gonfig.GetConf(configFile, &rawConfig)
+
+	instance = configuration{
+		Port:              rawConfig.Port,
+		StellarSeed:       rawConfig.StellarSeed,
+		JaegerUrl:         rawConfig.JaegerUrl,
+		JaegerServiceName: rawConfig.JaegerServiceName,
+		MaxConcurrency:    rawConfig.MaxConcurrency,
+	}
+
+	instance.AutoFlushPeriod,err = time.ParseDuration(rawConfig.AutoFlushPeriod)
+
+	if err != nil {
+		return configuration{}, errors.Errorf("Error parsing AutoFlushPeriod setting: " + err.Error())
+	}
+
+	if err != nil {
+		return configuration{}, errors.Errorf("Error parsing configuration: " + err.Error())
+	}
+
+	return instance,nil
+}

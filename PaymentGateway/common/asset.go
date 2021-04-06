05f7576d431d0b27e2cb32130cedc9e0c105dbf4
Michael Kanevsky <michael@adaptivelogic.net>
2021-04-06 16:20:09 +0300

    missing from commit

--- PaymentGateway/common/asset.go
+++ PaymentGateway/common/asset.go
@@ -8,12 +8,18 @@ import (
 const PPTokenIssuerAddress = "GCW3GHZEZCKR5QAXYSLJ6PB2Y2VUMQ75VKJNYCSTEFDNRQHJFF3U65IY"
 const PPTokenAssetName = "pptoken"
 const PPTokenMinAllowedBalance = 10
-const PPTokenUnitPrice = 1e-6 // uPP
+const PPTokenUnitPrice = 1e-3 // uPP
 
 func PPTokenToString(amount TransactionAmount) string {
 	return strconv.FormatFloat(PPTokenUnitPrice*float64(amount),'f', 7,64)
 	//return strconv.FormatUint(uint64(amount), 10)
 }
+
+func PPTokenToNumeric(transactionAmount float64) float64 {
+	return PPTokenUnitPrice*transactionAmount
+}
+
+
 
 func MicroPPToken2PPtoken(micro float64 ) TransactionAmount {
 	return TransactionAmount(math.Round(micro/PPTokenUnitPrice))

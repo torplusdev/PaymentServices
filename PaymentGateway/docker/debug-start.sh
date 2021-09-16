c1d88be8d2ebd2c4b90aa5547ce2f72fd786cb01
Marat Tukhbatullin <tumarsal@gmail.com>
2021-09-16 18:37:34 +0300

    add http handler

--- PaymentGateway/docker/debug-start.sh
+++ PaymentGateway/docker/debug-start.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+
+function ppchtest {
+    while true; do {
+        echo -en "HTTP/1.1 200 OK\r\n$(date)\r\n\r\n<h1>8080 port handler</h1>\r\n\r\n" |  nc -q 1 -l 8080; 
+    }
+    done 
+}
+
+ppchtest &
 /pg-docker-entrypoint.sh &
 /tor-docker-entrypoint.sh

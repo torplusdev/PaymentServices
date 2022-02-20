0f94740d66c5c9123851da16d99a8906bef011a3
Marat Tukhbatullin <tumarsal@gmail.com>
2022-02-20 23:32:02 +0300

    Ignore https

--- PaymentGateway/common/utils.go
+++ PaymentGateway/common/utils.go
@@ -3,6 +3,7 @@ package common
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -70,14 +71,14 @@ func HttpPostWithContext(ctx context.Context, url string, body io.Reader) (*http
 func HttpPostWithoutContext(url string, body io.Reader) (*http.Response, error) {
 	req, _ := http.NewRequest("POST", url, body)
 	req.Header.Add("Content-Type", "application/json")
-
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	return http.DefaultClient.Do(req)
 }
 
 func HttpPostWithoutResponseContext(url string, body io.Reader) error {
 	req, _ := http.NewRequest("POST", url, body)
 	req.Header.Add("Content-Type", "application/json")
-
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
